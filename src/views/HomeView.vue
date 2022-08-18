fa5b780815cf88cab33df8738ba6ce978eda3f02
sooraj-ngp <sooraj@ngpwebsmart.com>
2022-08-18 17:58:50 +0530

    Created new project and added display name dynamically and arithmetic operations.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Hello!"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from '@/components/ArithmeticOperations.vue'
 
 export default {
   name: 'HomeView',
   components: {
     HelloWorld
+  },
+  data(){
+    return {
+      name: ''
+    }
   }
 }
 </script>
